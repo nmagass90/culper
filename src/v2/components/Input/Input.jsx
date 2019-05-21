95eee054fe53a092162bfa3edaba3225b0ef38af
joshmfranklin <josh@truss.works>
2019-05-21 10:26:53 -0400

    destructure input component

--- src/v2/components/Input/Input.jsx
+++ src/v2/components/Input/Input.jsx
@@ -2,23 +2,55 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import uuid from 'uuid'
 
-function Input({ label, type, maxLength }) {
-  const uid = uuid.v4()
+export default class Input extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  return (
-    <div>
-      <label
-        htmlFor={uid}
-      >
-        {label}
-      </label>
-      <input
-        type={type}
-        id={uid}
-        maxLength={maxLength}
-      />
-    </div>
-  )
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    })
+  }
+
+  render() {
+    const uid = uuid.v4()
+
+    const { label, type, maxLength } = this.props
+
+    const { value } = this.state
+
+    return (
+      <div>
+        <label
+          htmlFor={uid}
+        >
+          {label}
+        </label>
+        <input
+          type={type}
+          id={uid}
+          maxLength={maxLength}
+          value={value}
+          onChange={this.handleChange}
+        />
+      </div>
+    )
+  }
 }
 
-export default Input
+Input.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  maxLength: PropTypes.number,
+}
+
+Input.defaultProps = {
+  label: '',
+  type: 'text',
+  maxLength: '',
+}
